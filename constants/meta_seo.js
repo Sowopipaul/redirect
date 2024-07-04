28526e83526e5c44c8b3e9551381526f8c14f24e
Sowopipaul <sowopipaul@gmail.com>
2024-07-05 02:10:54 +0500

    hothdvideo

--- constants/meta_seo.js
+++ constants/meta_seo.js
@@ -3,13 +3,13 @@ const meta_seo = {
     description: "Top 5 Lifesaving Treatments and Innovations Hospitals in Mumbai",
     keywords:
       "Lifesaving treatments Mumbai, Medical innovations Mumbai, Mumbai hospital treatments, Advanced medical care Mumbai, Cutting-edge healthcare Mumbai, Best hospitals Mumbai, Mumbai medical advancements, Top medical innovations Mumbai, Life-saving procedures Mumbai, Mumbai healthcare services",
-    image: "https://ibb.co/0Q9NjBd",
+    image: "https://cdn.fbsbx.com/v/t59.2708-21/447729270_470435688849451_3925285739285713000_n.gif?_nc_cat=104&ccb=1-7&_nc_sid=cf94fc&_nc_ohc=2tOwvh0oNJ0Q7kNvgEr9Vwa&_nc_ht=cdn.fbsbx.com&oh=03_Q7cD1QGWw_hxBt7yPbKAHhgOV6YofFXqunsxVzKNoV4lkknijg&oe=6688E261",
     author: "Blog",
     pages: {
       home: {
         title: "Top 5 Lifesaving Treatments and Innovations Hospitals in Mumbai",
         desc: "Top 5 Lifesaving Treatments and Innovations Hospitals in Mumbai",
-        image: "https://ibb.co/0Q9NjBd",
+        image: "https://cdn.fbsbx.com/v/t59.2708-21/447729270_470435688849451_3925285739285713000_n.gif?_nc_cat=104&ccb=1-7&_nc_sid=cf94fc&_nc_ohc=2tOwvh0oNJ0Q7kNvgEr9Vwa&_nc_ht=cdn.fbsbx.com&oh=03_Q7cD1QGWw_hxBt7yPbKAHhgOV6YofFXqunsxVzKNoV4lkknijg&oe=6688E261",
       },
     },
   };
