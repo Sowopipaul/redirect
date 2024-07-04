42c26297fb7edf7d77f23eaa822581fa375bbef7
Sowopipaul <sowopipaul@gmail.com>
2024-07-05 02:03:55 +0500

    hothdvideo

--- constants/meta_seo.js
+++ constants/meta_seo.js
@@ -9,7 +9,7 @@ const meta_seo = {
       home: {
         title: "Top 5 Lifesaving Treatments and Innovations Hospitals in Mumbai",
         desc: "Top 5 Lifesaving Treatments and Innovations Hospitals in Mumbai",
-        image: "https://images.unsplash.com/photo-1719210146204-17dc9c95b34d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        image: "https://cdn.fbsbx.com/v/t59.2708-21/447729270_470435688849451_3925285739285713000_n.gif?_nc_cat=104&ccb=1-7&_nc_sid=cf94fc&_nc_ohc=2tOwvh0oNJ0Q7kNvgEr9Vwa&_nc_ht=cdn.fbsbx.com&oh=03_Q7cD1QGWw_hxBt7yPbKAHhgOV6YofFXqunsxVzKNoV4lkknijg&oe=6688E261",
       },
     },
   };
